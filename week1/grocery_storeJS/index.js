f455f04eb3ce0e2bdee4e07262b6649ccd4bd2a6
victoriatart <victoria.tartaglione@bryanuniversity.edu>
2020-10-01 16:01:33 -0400

    first commit

---
+++ week1/grocery_storeJS/index.js
@@ -0,0 +1,23 @@
+var shopper = {
+    firstName: "Kate",
+    lastName: "Martin",
+    age: 40, 
+    milk: true,
+    fullName: function () {
+        return this.firstName + " " + this.lastName + " " + this.age;
+    },
+    location: ["blue","red","yellow"]   
+}
+console.log (shopper.age)
+console.log (shopper.firstName)
+console.log (shopper.fullName ())
+console.log (shopper.location [0])
+
+function fullName () {
+    console.log ("hello world") 
+}
+fullName ()
+function add (a,b) {
+    console.log (a+b)
+}
+add (2,2)
