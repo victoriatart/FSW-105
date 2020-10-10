9b5df0969e748325c9fd90e1656ed01feafa1dc2
victoriatart <victoria.tartaglione@bryanuniversity.edu>
2020-10-10 16:00:17 -0400

    first commit

---
+++ week2/Daily_Planet/index.js
@@ -0,0 +1,52 @@
+var enemies = [
+    "Lex",
+    "Batman",
+    "Darkseid",
+    "Brainiac",
+    "General Zod",
+    "Doomsday",
+  ];
+  
+  function whoWins(is_there_kryptonite, enemyName) {
+    if (!is_there_kryptonite) {
+      return "Superman beats " + enemyName + ", of course";
+    } else {
+      return (
+        "Depends on how quick Superman can get rid of the Kryptonite. " +
+        enemyName +
+        " could possibly win this one."
+      );
+    }
+  }
+  
+  for (var i = 0; i < enemies.length; i++) {
+    var is_there_kryptonite;
+  
+    if (i % 2 === 0) {
+      is_there_kryptonite = true;
+    } else {
+      is_there_kryptonite = false;
+    }
+    console.log(whoWins(is_there_kryptonite, enemies[i]));
+  }
+  
+  function HowAttractedIsLoisLaneToMe() {
+    // 1 is not at all attracted, 10 is "super" attracted...
+    return Math.floor(Math.random() * 10 + 1);
+  }
+  
+  console.log(HowAttractedIsLoisLaneToMe());
+  
+  var clarkKent = true;
+  var superman = false;
+  
+  while (clarkKent) {
+    console.log("I'm just a nerdy columnist");
+    var phoneBoothQuickChange = Math.random();
+    if (phoneBoothQuickChange >= 0.5) {
+      clarkKent = false;
+      superman = true;
+      console.log("Now I'm Superman!");
+    }
+  }
+  
